bf3799dbf3312f3817437928c9b8cd72fe35f57b
Kasik <death101@2die4.com>
2013-10-24 21:21:04 -0500

    Added VideoBull

---
+++ plugin.video.vdeobull/resources/src/scripts/delFavsL.py
@@ -0,0 +1,49 @@
+import urllib,urllib2,re,cookielib,xbmcplugin,xbmcgui,xbmcaddon,time,socket,string,os,shutil
+from t0mm0.common.addon import Addon
+
+#SET DIRECTORIES
+addon_id = 'plugin.video.vdeobull'
+selfAddon = xbmcaddon.Addon(id=addon_id)
+addon = Addon(addon_id)
+datapath = addon.get_profile()
+
+favpath=os.path.join(datapath,'Favourites')
+tvfav=os.path.join(favpath,'Live')
+FavFile=os.path.join(tvfav,'LiveFav') 
+
+def delFAVS(url,title,mode,thumb,plot,type):
+    Favs=re.compile('url="(.+?)",name="(.+?)",mode="(.+?)",thumb="(.+?)",plot="(.+?)",type="(.+?)"').findall(open(FavFile,'r').read())
+    if not str(Favs).find(title):
+        xbmc.executebuiltin("XBMC.Notification([B][COLOR green]TV Rule[/COLOR][/B],[B][COLOR orange]"+title+"[/COLOR]not in Favourites.[/B],1000,"")")
+    if len(Favs)<=1 and str(Favs).find(title):
+        os.remove(FavFile)
+        xbmc.executebuiltin("Container.Refresh")
+    if os.path.exists(FavFile):
+        for url,name,mode,thumb,plot,type in reversed(Favs):
+            if title == name:
+                Favs.remove((url,name,mode,thumb,plot,type))
+                os.remove(FavFile)
+                for url,name,mode,thumb,plot,type in Favs:
+                    try:
+                        open(FavFile,'a').write('url="%s",name="%s",mode="%s",thumb="%s",plot="%s",type="%s",'%(url,name,mode,thumb,plot,type))
+                        xbmc.executebuiltin("Container.Refresh")
+                        xbmc.executebuiltin("XBMC.Notification([B][COLOR orange]"+title+"[/COLOR][/B],[B]Removed from Favourites[/B],1000,"")")
+                    except: pass
+    else: xbmc.executebuiltin("XBMC.Notification([B][COLOR green]VideoBull[/COLOR][/B],[B]You Have No Favourites to delete[/B],1000,"")")
+
+bits = sys.argv[1].split(',')
+print "BaseUrl= "+sys.argv[0]
+url = bits[0].replace("[(u'",'').replace("'",'')
+print "Url= "+url
+title = bits[1].replace("'[B][COLOR green]",'').replace("[/COLOR][/B]')]",'').strip()
+print "name= "+title
+mode = bits[2].replace(" ",'').replace("'",'')
+print "Mode= "+mode
+thumb = bits[3].replace(" ",'').replace("'",'')
+print "Thumb= "+thumb
+plot = bits[4].replace("'",'').replace('"','').replace("(",'').replace(")",'').replace("]",'')
+print "Plot= "+plot
+type=bits[5].replace(" ",'').replace("'",'').replace('"','').replace("(",'').replace(")",'').replace("]",'')
+print "Type= "+type
+
+delFAVS(url.replace('[(',''),title.replace("'",'').replace(')]',''),mode,thumb,plot,type)
---
+++ zips/plugin.video.vdeobull/resources/src/scripts/settings.py
@@ -0,0 +1,34 @@
+import xbmc, xbmcaddon
+addon_id = 'plugin.video.vdeobull'
+selfAddon = xbmcaddon.Addon(id=addon_id)
+
+
+
+def getHomeItems():
+    d=[]
+    for x in range(24): 
+        d.append(None);
+        itemid = str(x + 1)
+        if selfAddon.getSetting("home_item_" +itemid+ "_enabled")== "true":
+            d[x]=int(selfAddon.getSetting("home_item_" + itemid))
+    return d
+
+def getRefreshRequiredSettings():
+    s=[]
+    s.append(selfAddon.getSetting("meta-view"))
+    s.append(selfAddon.getSetting("meta-view-tv"))
+    s.append(selfAddon.getSetting("groupfavs"))
+    s.append(selfAddon.getSetting("con-view"))
+    s.append(selfAddon.getSetting("xpr-view"))
+    return s
+
+def openSettings():
+    d = getHomeItems()
+    s = getRefreshRequiredSettings()
+    selfAddon.openSettings()
+    dnew = getHomeItems()
+    snew = getRefreshRequiredSettings()
+    if d != dnew or s != snew:
+        xbmc.executebuiltin("XBMC.Container.Refresh")  
+
+if  __name__ == "__main__": openSettings()
